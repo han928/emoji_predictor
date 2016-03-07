c6cc0b5bb504ca8b52bf905e2688c5344414523d
han928 <tzunghan.lai@gmail.com>
2016-03-06 20:23:57 -0800

    add spark script and remove filter words

---
+++ spark.py
@@ -0,0 +1,13 @@
+from pyspark import SparkContext
+import json
+
+sc = SparkContext()
+
+
+
+
+
+if __name__ == '__main__':
+    print sc.textFile('data/twitter_dump.txt').filter(lambda tw: len(tw)>1)\
+        .filter(lambda tw: 'created_at' in tw).map(lambda tw: tw.strip())\
+        .map(lambda tw: json.loads(tw) ).map(lambda tw: tw['text']).count()
